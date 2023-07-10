67101757f32318168babec52a88586d99241c6ae
dianarentea <rentea88@gmail.com>
2023-07-10 13:06:29 +0300

    created a table in description.html

--- Pharmacy/src/main/java/com/example/Pharmacy/controller/PillController.java
+++ Pharmacy/src/main/java/com/example/Pharmacy/controller/PillController.java
@@ -5,15 +5,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class PillController {
 
     @GetMapping(value="/description")
-    public void description(Model model)
+    public String description(Model model)
     {
         Pill p1=new Pill("Paracetamol",30, 15,"dureri de cap");
+        Pill p2=new Pill("Decasept", 15, 30, "dureri de gat");
+        Pill p3= new Pill("Magnerot", 80, 20, "contracturi musculare");
+        List<Pill> pillList= List.of(p1, p2,p3);
+        model.addAttribute("pillList", pillList);
 
-        model.addAttribute("pill", p1);
-
+        return "description";
     }
 }
