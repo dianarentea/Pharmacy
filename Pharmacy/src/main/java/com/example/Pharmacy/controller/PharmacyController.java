6775a33b922aaadee6e7236dec7f030725d0c281
DGSergiu <desaga.sergiug@gmail.com>
2023-07-14 15:16:11 +0300

    Update

--- Pharmacy/src/main/java/com/example/Pharmacy/controller/PharmacyController.java
+++ Pharmacy/src/main/java/com/example/Pharmacy/controller/PharmacyController.java
@@ -5,6 +5,8 @@ import com.example.Pharmacy.model.Pill;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.*;
@@ -29,14 +31,26 @@ public class PharmacyController {
                                 .collect(Collectors.toList());
 
 
-
-
         model.addAttribute("pharmacyList",sortedByCategory);
 
 
         return "pharmacy";
 
+
+
+    }
+    @GetMapping(value="/pharmacyForm")
+    public String getPharmacyForm(Model model)
+    {
+        model.addAttribute("pharmacy", new Pharmacy());
+        return "pharmacyForm";
     }
 
+    @PostMapping(value="/addPharmacy")
+    public String submitPharmacy(@ModelAttribute("pharmacy") Pharmacy pharmacy)
+    {
+        System.out.println(pharmacy.toString());
+        return null;
+    }
 
 }
