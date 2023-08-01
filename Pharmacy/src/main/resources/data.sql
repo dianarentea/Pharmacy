30d86690ae8675a2603d9a8cb668612688e015a2
dianarentea <rentea88@gmail.com>
2023-08-01 10:30:19 +0300

    added some styling

--- Pharmacy/src/main/resources/data.sql
+++ Pharmacy/src/main/resources/data.sql
@@ -6,7 +6,7 @@ values ('XY456', 'Catena');
 insert into t_pill(name, price , stock_number, description,image_name, id_pharmacy)
 values ('Aspenter',12, 40, 'tensiune arteriala','p1.jpeg',1);
 insert into t_pill(name, price , stock_number, description,image_name, id_pharmacy)
-values ('Nurofen',20,15,'dureri de cap','p2.jpg',1);
+values ('Nurofen',20,15,'dureri de cap','p2.jpeg',1);
 insert into t_pill(name, price , stock_number, description,image_name, id_pharmacy)
 values ('Augumentin',22,30,'migrene','p3.jpeg',2);
 insert into t_pill(name, price , stock_number, description,image_name, id_pharmacy)
@@ -18,4 +18,4 @@ values ('Litorsal',27,43,'deshidratare','p6.jpeg',1);
 insert into t_pill(name, price , stock_number, description,image_name, id_pharmacy)
 values ('Olinth',27,43,'raceala','p7.jpeg',2);
 insert into t_pill(name, price , stock_number, description,image_name, id_pharmacy)
-values ('Espumisan',27,43,'balonari','p8.jpg',1);
+values ('Espumisan',27,43,'balonari','p8.jpeg',1);
